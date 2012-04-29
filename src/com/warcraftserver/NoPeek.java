d3307939e62362945d6ef15f894c0bf388ae6c57
DarkCloud784 <mrkrodriguez@gmail.com>
2012-04-29 16:52:32 -0500

    added plugin.yml

--- src/com/warcraftserver/NoPeek.java
+++ src/com/warcraftserver/NoPeek.java
@@ -1,4 +1,4 @@
-package src;
+package com.warcraftserver;
 
 import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -6,18 +6,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class NoPeek extends JavaPlugin {
     Logger log = Logger.getLogger("Minecraft");
-    private final NoPeekListener Listener = new NoPeekListener();
+    private final NoPeekListener listener = new NoPeekListener();
     public static String ColorList;
     public static String color;
     public static String message;
     public static String enableOrDisable;
 }
 
-
 public void onDisable() {
 this.log.info("NP (No Peek) has been disabled!")
 
 }
+
+@Override
 
 public void onEnable() {
 PluginManager pm = getServer().getPluginManager();
