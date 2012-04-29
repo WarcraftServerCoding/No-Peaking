f63ebad0574c2bf7ee2dadf2d9527d6248826f2b
DarkCloud784 <mrkrodriguez@gmail.com>
2012-04-29 18:25:04 -0500

    How about we check the config message before trying to run it.

--- src/com/warcraftserver/NoPeak.java
+++ src/com/warcraftserver/NoPeak.java
@@ -2,6 +2,7 @@
 package com.warcraftserver;
 
 import java.util.logging.Logger;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventPriority;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,6 +28,7 @@ public class NoPeak extends JavaPlugin {
         this.getConfig();
         this.getConfig().options().header("Change the message: 'No Peaking at our plugins!'");
         message = this.getConfig().getString("Access Message", "No Peaking!");
+        enableOrDisable = this.getConfig().getString("Send Output", "true");
         this.saveConfig();
         
         this.log.info("[No Peak] Has been enabled!");
